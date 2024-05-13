783c83ec9a17186f90c05a6828a9d7bb4f78c30d
Guillaume CUADRADO <gucd@oticonmedical.com>
2024-05-13 18:02:34 +0200

    Backbone for Wifi Setup

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,50 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
-use bsp::entry;
-use defmt::*;
-use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
-use panic_probe as _;
+// Ensure we halt the program on panic (if we don't mention this crate it won't
+// be linked)
+use panic_halt as _;
 
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
+// Alias for our HAL crate
+use rp2040_hal as hal;
 
-use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
-};
+// A shorter alias for the Peripheral Access Crate, which provides low-level
+// register access
+use hal::pac;
 
-#[entry]
+// Some traits we need
+use hal::fugit::RateExtU32;
+use hal::Clock;
+
+// UART related types
+use hal::uart::{DataBits, StopBits, UartConfig};
+
+/// The linker will place this boot block at the start of our program image. We
+/// need this to help the ROM bootloader get our code up and running.
+/// Note: This boot block is not necessary when using a rp-hal based BSP
+/// as the BSPs already perform this step.
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+
+/// External high-speed crystal on the Raspberry Pi Pico board is 12 MHz. Adjust
+/// if your board has a different frequency
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+/// Entry point to our bare-metal application.
+///
+/// The `#[rp2040_hal::entry]` macro ensures the Cortex-M start-up code calls this function
+/// as soon as all global variables and the spinlock are initialised.
+#[rp2040_hal::entry]
 fn main() -> ! {
-    info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    let sio = hal::Sio::new(pac.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -41,36 +52,74 @@ fn main() -> ! {
         &mut pac.RESETS,
         &mut watchdog,
     )
-    .ok()
     .unwrap();
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    let pins = bsp::Pins::new(
+    let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    //
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead.
-    // One way to do that is by using [embassy](https://github.com/embassy-rs/embassy/blob/main/examples/rp/src/bin/wifi_blinky.rs)
-    //
-    // If you have a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here. Don't forget adding an appropriate resistor
-    // in series with the LED.
-    let mut led_pin = pins.led.into_push_pull_output();
+    // UART Periph & pins Configuration
+    let uart_pins = (
+        // UART TX (characters sent from RP2040) on pin 1 (GPIO0)
+        pins.gpio0.into_function(),
+        // UART RX (characters received by RP2040) on pin 2 (GPIO1)
+        pins.gpio1.into_function(),
+    );
+    let uart = hal::uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+        .enable(
+            UartConfig::new(115200.Hz(), DataBits::Eight, None, StopBits::One),
+            clocks.peripheral_clock.freq(),
+        )
+        .unwrap();
+
+    // Initialize WIFI connection
+    // Restart: "AT+RST\r\n"
+    uart.write_full_blocking(b"AT+RST\r\n");
+
+    // SSID config
+    let ssid = b"your_ssid";
+    let password = b"your_password";
+    let mut cmd = [0u8; 50]; // Adjust the size as needed
+
+    let prefix = b"AT+CWJAP=\"";
+    let infix = b"\",\"";
+    let postfix = b"\"\r\n";
+
+    let mut index = 0;
+
+    let mut copy_into_cmd = |src: &[u8]| {
+        cmd[index..index + src.len()].copy_from_slice(src);
+        index += src.len();
+    };
+
+    copy_into_cmd(prefix);
+    copy_into_cmd(ssid);
+    copy_into_cmd(infix);
+    copy_into_cmd(password);
+    copy_into_cmd(postfix);
+
+    uart.write_full_blocking(&cmd[..index]);
+
+    delay.delay_ms(3000_u32);
+    // STA config: "AT+CWMODE=1\r\n"
+    uart.write_full_blocking(b"AT+CWMODE=1\r\n");
+    delay.delay_ms(1500_u32);
+    // IP address: "AT+CIFSR\r\n"
+    uart.write_full_blocking(b"AT+CIFSR\r\n");
+    delay.delay_ms(1500_u32);
+    // Several connections: "AT+CIPMUX=1\r\n"
+    uart.write_full_blocking(b"AT+CIPMUX=1\r\n");
+    delay.delay_ms(1500_u32);
+    // Server config: "AT+CIPSERVER=1,80\r\n"
+    uart.write_full_blocking(b"AT+CIPSERVER=1,80\r\n");
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        cortex_m::asm::wfi();
     }
 }
 
